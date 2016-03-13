c7e727d581d1dc58d52b8462e47604851b026179
Michael Carroll <carroll.michael@gmail.com>
2016-03-13 20:02:15 +0000

    Javascript is hard

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
 var replace = require('gulp-replace');
 
+
 gulp.task('deploy', function() {
 	return gulp.src("./app/**/*")
-	    .pipe(replace('588036218022182','586301318195672'))
+		.pipe(replace('588036218022182','586301318195672'),
+				{skipBinary: true})
 		.pipe(deploy({branch: 'master'}))
 });
