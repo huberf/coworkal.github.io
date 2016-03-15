843836d59fb76e74010a79b82b0ef6693c0f9d41
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-03-15 12:04:24 -0500

    Fixing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var replace = require('gulp-replace');
 
 gulp.task('deploy', function() {
 	return gulp.src(["./app/**/*", "!./app/**/*.jpg", "!./app/**/*.png"])
-		.pipe(replace('588036218022182','586301318195672'),
-				{skipBinary: true})
+		.pipe(replace('588036218022182','586301318195672') /*,
+				{skipBinary: true} */)
 		.pipe(deploy({branch: 'master'}))
 });
